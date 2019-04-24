0ae5ca423921310cb5e852c8958670d1d6b5c796
amudak <amudak01@gmail.com>
2019-04-24 10:00:10 +0530

    Create allocation.php

---
+++ allocation.php
@@ -0,0 +1,29 @@
+
+<?php
+include("header.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body align="center">
+<div align="center">
+<form id="form1" name="form1" method="post" action="">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+<p><a href="staffallocation.php#form1"><font color="#990000">1st evaluation</font></a></p>
+<p><a href="staffallocation_2.php#form1"><font color="#0000FF">2nd evaluation</font></a></p>
+<p><a href="staffallocation_3.php#form1"><font color="#FF0000">3rd evaluation</font></a></p>
+</form>
+</div>
+</body>
+</html>
+<?php
+include("footer.php");
+?>
