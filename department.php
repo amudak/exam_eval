8f8e5da69aed3402300b0b0c632cec26afbf99a7
amudak <amudak01@gmail.com>
2019-04-25 16:17:03 +0530

    Update department.php

--- department.php
+++ department.php
@@ -1,3 +1,8 @@
+
+<?php
+
+	include("header.php");
+	?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -23,16 +28,23 @@ function valid()
 	</script>
 <body>
 <form id="form1" name="form1" method="post" action="">
-  <table width="273" border="0">
+<p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+
+  <table width="273" border="0" align="center" cellpadding="10" bgcolor="#CCCCFF">
     <tr>
-      <td width="113">Department Name</td>
+      <td width="113"><font color="#000000">Department Name</font></td>
       <td width="144"><label for="name"></label>
       <input type="text" name="name" id="name" /></td>
       <td>  <div style="visibility:hidden" id="a"><font color="#FF0000">Invalid Entry</font></div></td>
     </tr>
     <tr>
       <td colspan="2"><div align="center">
-        <input type="submit" name="register" id="register" value="Register" onclick="return valid()" />
+        <input type="submit" name="register" id="register" value="Register" onclick="return valid()" style="background:#CC6"/>
       </div></td>
     </tr>
   </table>
@@ -56,4 +68,6 @@ if($qry>0)
 	}
 }
 
-?>
+
+	include("footer.php");
+	?>
