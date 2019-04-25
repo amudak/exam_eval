34e4cb0a5a67a2b17cbaf8a53d16737aa6aaddef
amudak <amudak01@gmail.com>
2019-04-25 16:16:33 +0530

    Update college_home.php

--- college_home.php
+++ college_home.php
@@ -1,3 +1,9 @@
+
+
+<?php
+
+	include("College_header.php");
+	?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -5,8 +11,8 @@
 <title>Untitled Document</title>
 </head>
 
-<body>
-<p><a href="staffreg.php">Staff Registartion</a></p>
+<body align="center">
+<!--<p><a href="staffreg.php">Staff Registartion</a></p>
 <p><a href="staffview.php">View staff</a></p>
 <p><a href="student.php">Student Registration</a></p>
 <p><a href="studentview.php">View Student</a></p>
@@ -14,7 +20,7 @@
 <p><a href="apply_exam.php">Apply Exam</a></p>
 <p><a href="upload_anssheet.php">Upload Answersheet</a></p>
 <p><a href="papernotificationview.php">View Notification</a></p>
-<p><a href="login.php">Logout</a></p>
+<p><a href="login.php">Logout</a></p>-->
 
 </body>
 </html>
